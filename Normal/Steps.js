44d379488bdc9d3e9ab50262365c7899325b1f2c
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-18 22:29:41 +0600

    Solved 1 to 20 problem

---
+++ Normal/Steps.js
@@ -0,0 +1,39 @@
+//Step 8
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// console.log(character);
+// let secondCharacter;
+
+//Step 9
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// let secondCharacter;
+// console.log(secondCharacter);
+
+
+//Step 9
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// let secondCharacter = "Test";
+// console.log(secondCharacter);
+
+//step 10
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// let secondCharacter;
+// secondCharacter = "Test";
+// console.log(secondCharacter);
+
+//Step 20 
+// let character = 'Hello';
+// let count = 8;
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// console.log(rows[0]);
+
+//step 21
+
+
