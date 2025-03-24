7d578342d17f15672ad389d09cd4ad7a6b7d1fb5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-24 23:27:21 +0600

    Solved 42 steps

--- Normal/Steps.js
+++ Normal/Steps.js
@@ -63,6 +63,25 @@
 // const rows = [];
 
 //step 32 to 41
-for(const row of rows){
+// for(const row of rows){
   
+// }
+
+
+//Step 42
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
 }
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row;
+
+}
+
+console.log(result);
