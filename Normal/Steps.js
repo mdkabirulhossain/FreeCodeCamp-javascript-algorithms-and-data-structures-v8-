ebca5ad0e7e6129e0b5d9c6286c134fbb5aeb3a8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-26 23:37:48 +0600

    Sovled step 43

--- Normal/Steps.js
+++ Normal/Steps.js
@@ -89,21 +89,36 @@
 
 //Step 43
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i);
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + row  + "\n";
+  
+// }
+
+// console.log(result);
+
+// step 43
 const character = "#";
 const count = 8;
 const rows = [];
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);
+  rows.push(character);
 }
 
 let result = ""
 
 for (const row of rows) {
-  result = result + row  + "\n";
-  
+  result = result + row + "\n";
 }
 
 console.log(result);
-
-
